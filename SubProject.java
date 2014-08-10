b14217f963c5f90a9cbefb36f16c0f6afcab5228
Md Alam <mdalam1218@gmail.com>
2014-08-10 00:07:21 -0400

    home work 2 subtract project

---
+++ SubProject.java
@@ -0,0 +1,21 @@
+package com.java.sub;
+
+public class SubProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int firstNumber=50;
+			int secandNumber=10;
+			int total;
+			total=firstNumber-secandNumber;
+			
+			System.out.print("Subtrac resalt is is:    ");
+			System.out.println(total);
+			
+	
+
+
+	}
+
+}
